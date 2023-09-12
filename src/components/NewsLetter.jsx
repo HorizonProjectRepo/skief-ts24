850c16ac1197c35479fceedce277d95e67792471
Md-TarikulIslam <nayon1820@gmail.com>
2023-09-12 17:13:53 +0600

    12th Sep, 2023

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -1,84 +1,19 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import './Newsletter.css'
 
 const NewLetter = () => {
-  const [email, setEmail] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
 
-
-  const subscribeToMailchimp = async (email, firstName, lastName) => {
-    const API_KEY = '7f35a93617657836059c4f6bf0465da7-us14';
-    const LIST_ID = '6b37acabd9';
-  
-    const data = {
-      email_address: email,
-      status: 'subscribed',
-      merge_fields: {
-        FNAME: firstName,
-        LNAME: lastName,
-      },
-    };
-  
-    try {
-      const response = await axios.post(
-        `https://us14.api.mailchimp.com/3.0/lists/6b37acabd9/members`,
-        data,
-        {
-          headers: {
-            Authorization: `apikey ${API_KEY}`,
-          },
-        }
-      );
-  
-      console.log('Successfully subscribed', response.data);
-      // Handle success
-    } catch (error) {
-      console.error('Error subscribing', error);
-      // Handle error
-    }
-  };
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handleFirstNameChange = (e) => {
-    setFirstName(e.target.value);
-  };
-
-  const handleLastNameChange = (e) => {
-    setLastName(e.target.value);
-  };
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  subscribeToMailchimp(email, firstName, lastName);
-};
+ 
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={handleEmailChange}
-      />
-      <input
-        type="text"
-        placeholder="First Name"
-        value={firstName}
-        onChange={handleFirstNameChange}
-      />
-      <input
-        type="text"
-        placeholder="Last Name"
-        value={lastName}
-        onChange={handleLastNameChange}
-      />
-      <button type="submit">Subscribe</button>
-    </form>
-  );
+
+    <div className='h-96'>
+
+    </div>
+  )
+
 };
+
 
 export default NewLetter;
